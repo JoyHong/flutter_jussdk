8ab9ff1fa95fc67f909e120f3a71f3ffa52908f6
joy.chen <joy.chen@justalk.com>
2024-06-27 15:44:19 +0800

    refactor: example 验证通过

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,52 @@
+import 'dart:ffi' as ffi;
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_jussdk/flutter_mtc.dart' as flutter_mtc;
+import 'package:flutter_jussdk/flutter_mtc_bindings_generated.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
 
-import 'package:flutter_jussdk/flutter_jussdk.dart' as flutter_jussdk;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  Directory dir = await getApplicationSupportDirectory();
+  Directory logDir = await Directory('${dir.path}/log').create(recursive: true);
+  flutter_mtc.Mtc_CliCfgSetLogDir(logDir.path);
+  Directory profileDir = await Directory('${dir.path}/profile').create(recursive: true);
+  if (Platform.isWindows) {
+    final myUiEventCallable = ffi.NativeCallable<MyUiEvent>.listener(myUiEvent);
+    flutter_mtc.Mtc_CliInit(profileDir.path, myUiEventCallable.nativeFunction.cast<ffi.Void>());
+  } else {
+    flutter_mtc.Mtc_CliInit(profileDir.path, ffi.nullptr);
+  }
+
+  if (Platform.isAndroid) {
+    const EventChannel('com.jus.flutter_jusdk.MtcNotify')
+        .receiveBroadcastStream()
+        .listen((event) {});
+  } else {
+    flutter_mtc.Mtc_CliCbSetNotify(ffi.Pointer.fromFunction(mtcNotify, 0));
+  }
+
   runApp(const MyApp());
+}
+
+typedef MyUiEvent = ffi.Void Function(ffi.Pointer<ffi.Void> zEvntId);
+typedef MyUiEventPtr = ffi.Pointer<ffi.NativeFunction<MyUiEvent>>;
+
+void myUiEvent(ffi.Pointer<ffi.Void> zEvntId) {
+  // 发送到 ui 线程去执行 CliDrive
+  // SchedulerBinding.instance.addPostFrameCallback((_) {
+  flutter_mtc.Mtc_CliDrive(zEvntId);
+  // });
+  // return 0;
+}
+
+int mtcNotify(ffi.Pointer<ffi.Char> pcName, int zCookie, ffi.Pointer<ffi.Char> pcInfo) {
+  // pcName.cast<Utf8>().toDartString();
+  return 0;
 }
 
 class MyApp extends StatefulWidget {
@@ -15,60 +57,61 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> sumAsyncResult;
-
-  @override
-  void initState() {
-    super.initState();
-    sumResult = flutter_jussdk.sum(1, 2);
-    sumAsyncResult = flutter_jussdk.sumAsync(3, 4);
-  }
-
   @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
+      theme: ThemeData(),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Native Packages'),
+          title: const Text('Flutter_SDK'),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                Text(
-                  'sum(1, 2) = $sumResult',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue =
-                        (value.hasData) ? value.data : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: TextAlign.center,
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
+        body: Wrap(
+          spacing: 12,
+          children: [
+            button(text: '注册', onPressed: () {}),
+            button(text: '登录', onPressed: () async {
+              const String userType = MTC_USER_ID_USERNAME;
+              const String userName = 'dev.juphoon';
+              const String password = '198583';
+
+              // cliOpen 流程
+              flutter_mtc.Mtc_CliOpen(userType, userName);
+              flutter_mtc.Mtc_ProfResetProvision();
+              flutter_mtc.Mtc_UeDbSetIdTypeX(MTC_USER_ID_USERNAME);
+              flutter_mtc.Mtc_UeDbSetUserName(userName);
+              flutter_mtc.Mtc_UeDbSetPassword(password);
+              final packageInfo = await PackageInfo.fromPlatform();
+              flutter_mtc.Mtc_ProfDbSetAppVer(packageInfo.buildNumber);
+              flutter_mtc.Mtc_ProfSaveProvision();
+
+              // cliStart 流程
+              const appKey = '28ee4fa00244030f59334097';
+              flutter_mtc.Mtc_UeDbSetAppKey(appKey);
+              const router = 'Router:udp -h intl.router.justalkcloud.com -hb 74.207.243.39 -p 8000;udp -h 143.42.116.123 -p 8000;udp -h 172.105.36.56 -p 8000;udp -h 178.79.144.177 -p 8000;udp -h 148.153.85.44 -p 8000;http -h intl.router.justalkcloud.com -hb 74.207.243.39 -p 8080;http -h 143.42.116.123 -p 8080;http -h 172.105.36.56 -p 8080;http -h 178.79.144.177 -p 8080;http -h 148.153.85.44 -p 8080;';
+              flutter_mtc.Mtc_UeDbSetNetwork(router);
+              const accessEntry = '';
+              flutter_mtc.Mtc_UeDbSetEntry(accessEntry);
+              const deviceId = 'deviceId';
+              flutter_mtc.Mtc_CliApplyDevId(deviceId);
+              flutter_mtc.Mtc_CliStart();
+              flutter_mtc.Mtc_CliLogin(MTC_LOGIN_OPTION_PREEMPTIVE);
+            }),
+            button(text: '退出登录', onPressed: () {
+              flutter_mtc.Mtc_CliLogout();
+              // 退出登录成功后要清空 MtcUeDb 并 CliStop
+            }),
+            button(text: '自定义测试', onPressed: () {
+            })
+          ],
         ),
       ),
     );
+  }
+
+  Widget button(
+      {required String text, bool enabled = true, VoidCallback? onPressed}) {
+    return ElevatedButton(
+        onPressed: enabled ? onPressed : null,
+        child: Text(text, style: const TextStyle(fontSize: 18)));
   }
 }
