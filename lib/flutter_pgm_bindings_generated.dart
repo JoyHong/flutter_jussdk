ecb7f58f50ac0d28360ee78d5653a6ad7ab52d9a
Joy.Chen <joy.chen@justalk.com>
2024-08-06 13:32:18 +0800

    refactor: PGM bindings 类名修改

--- lib/flutter_pgm_bindings_generated.dart
+++ lib/flutter_pgm_bindings_generated.dart
@@ -12,17 +12,17 @@ import 'dart:ffi' as ffi;
 ///
 /// Regenerate bindings with `flutter pub run ffigen --config ffigen_pgm.yaml`.
 ///
-class FlutterMtcBindings {
+class FlutterPGMBindings {
   /// Holds the symbol lookup function.
   final ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName)
       _lookup;
 
   /// The symbols are looked up in [dynamicLibrary].
-  FlutterMtcBindings(ffi.DynamicLibrary dynamicLibrary)
+  FlutterPGMBindings(ffi.DynamicLibrary dynamicLibrary)
       : _lookup = dynamicLibrary.lookup;
 
   /// The symbols are looked up with [lookup].
-  FlutterMtcBindings.fromLookup(
+  FlutterPGMBindings.fromLookup(
       ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName)
           lookup)
       : _lookup = lookup;
