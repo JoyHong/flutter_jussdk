b1de673031ababd80768da0c7da0a50cacf62421
Joy.Chen <joy.chen@justalk.com>
2024-09-03 10:05:54 +0800

    refactor: 导入 drift

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_jussdk/flutter_jussdk_plugin_c_api.h>
+#include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterJussdkPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterJussdkPluginCApi"));
+  Sqlite3FlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
 }
